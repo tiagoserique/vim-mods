7dd9aa4719178584b10e627ce92a92d712d91530
tiagoserique <seriquevaladares29@gmail.com>
2020-04-01 05:30:24 -0300

    Fix errors

--- script.sh
+++ script.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-CAMINHO=$( pwd )
+pushd ~
+	if [ -d .vim ] && [ -a .vimrc ]
+	then
+		rm .vimrc
+		rm -rf .vim
+	fi
+
+	CAMINHO=$(find -name vim-mods)
+	cd $CAMINHO
+	CAMINHO=$(pwd)
+popd
+
+if [[ ! $(pwd) == $CAMINHO ]]
+then
+	cd $CAMINHO
+fi
+
 git submodule init
+
 git submodule update
+
 mv .vim* ~/
+
 mv script.sh ~/
+
 cd ~
-rm -rf $CAMINHO/vim-mods
+
+rm -rf $CAMINHO
+
 rm $0
